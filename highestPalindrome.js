2151a926796f6125fbc368a220d0e2220176f514
lutfiadnan92 <adnanlutfi92@gmail.com>
2023-06-28 09:50:45 +0700

    feat: update highest palindrome function

--- highestPalindrome.js
+++ highestPalindrome.js
@@ -1,3 +1,23 @@
-const highestPalindrome = (string, k) => {
-  const arrayNumber = string.split('')
+const result = string => {
+  const stringSplit = string.split('')
+  const stringLength = stringSplit.length;
+ 
+  let k = 0
+
+  for(let i= 0;i < stringLength/2; i++) {
+    if(stringSplit[i] === stringSplit[stringLength - i - 1])
+      continue
+
+    k += 1
+
+    if(stringSplit[i] < stringSplit[stringLength-i-1])
+      stringSplit[i] = stringSplit[stringLength-i-1]
+    else
+      stringSplit[stringLength-i-1] = stringSplit[i]
+  }
+
+  if(stringSplit[1] === stringSplit[2]) return stringSplit.join('')
+  else return -1
 }
+
+exports.result = result
