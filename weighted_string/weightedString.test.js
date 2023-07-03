65d56f4cb99038f2d35c5b10e9c43702ed5cc01f
lutfiadnan92 <adnanlutfi92@gmail.com>
2023-07-04 01:12:49 +0700

    test: add unit test for weighted string

--- weighted_string/weightedString.test.js
+++ weighted_string/weightedString.test.js
@@ -1,34 +1,33 @@
 const weightedString = require("./weightedString.services")
 
-test("is string lowercase?", () => {
-  const string = "a"
+test("string must be lowercase", () => {
+  const string = "ABC"
   const result = string.toLowerCase()
-  const output = "a"
+  const output = "abc"
   expect(result).toBe(output)
 })
 
-test("if string is uppercase convert to lowercase", () => {
-  const string = "A"
-  const result = string.toLowerCase()
-  const output = "a"
-  expect(result).toBe(output)
+test("string must be lowercase and group if it contains the same string", () => {
+  const string = "abbccc"
+  const result = weightedString.getAlphabetCharacters(string)
+  const output = ["a", "bb", "ccc"]
+  expect(result).toEqual(output)
 })
 
-test("if string is uppercase convert to lowercase", () => {
-  const string = "A"
-  const result = string.toLowerCase()
-  const output = "a"
-  expect(result).toBe(output)
-})
-
-test("check if the alphabet is not a-z", () => {
-  const string = '@'
-  const result = weightedString.isAlphabet(string)
+test("if the string is non-alphabethic, return false", () => {
+  const string = "@!$#"
+  const result = weightedString.getAlphabetCharacters(string)
   expect(result).toBeFalsy()
 })
 
-test("check the alphabet, a-z = 1-26", () => {
-  const string = ['abcdefghijklmnopqrstuvwxyz']
+test("if empty string", () => {
+  const string = ""
+  const result = weightedString.getAlphabetCharacters(string)
+  expect(result).toBeUndefined()
+})
+
+test("return the value of each character", () => {
+  const string = ["abcdefghijklmnopqrstuvwxyz"]
   const result = weightedString.sumOfQueries(string)
   const output = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -37,7 +36,7 @@ test("check the alphabet, a-z = 1-26", () => {
   expect(result).toEqual(output)
 })
 
-test("test_lowercase_string", () => {
+test("if string is lowercase then return yes", () => {
   const string = "abcdefghijklmnopqrstuvwxyz"
   const queries = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -49,10 +48,21 @@ test("test_lowercase_string", () => {
 })
 
 // Tests that the function works wtesth a string containing only lowercase letters
-test("test_uppercase_string", () => {
-  const string = "Abcd"
-  const queries = [1,2,3,4,5]
-  const expected = ["Yes","Yes","Yes","Yes","No"]
+test("if string is uppercase, convert to lowercase and return yes", () => {
+  const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const queries = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26,
+  ]
+  const expected = queries.map(() => "Yes")
   const result = weightedString.result(string, queries)
   expect(result).toEqual(expected)
+})
+
+test("if parameter input is empty or not string and array", () => {
+  const string = 1
+  const queries = '1'
+  const expectedError = "Invalid input: string must be a string and queries must be an array"
+  expect(weightedString.result).toThrow(expectedError)
+  expect(() => weightedString.result(string, queries)).toThrow(expectedError)
 })
